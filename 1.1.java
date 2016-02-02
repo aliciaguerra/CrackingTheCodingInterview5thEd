f276c0efbfaf91aa498279403f85ca08978cef17
Alicia Guerra <aiguerra@ucsd.edu>
2016-02-01 22:03:40 -0800

    Update 1.1.java

--- 1.1.java
+++ 1.1.java
@@ -5,6 +5,14 @@ public boolean isUniqueChars2(String str){
   //There are 128 unique ASCII characters. 
   //If the string has more than 128 characters, then some of the characters are not unique.
 boolean[] char_set = new boolean[256];
+  //There are 256 total ASCII characters (including extended ASCII characters).
 for(int i=0; i<str.length(); i++) {
 int val = str.charAt(i);
   //The charAt() method returns the character located at a string's specific index.
+  if(char_set[val]) { //Already found this char in string.
+  return false;
+  }
+  char_set[val]=true;
+}
+return true;
+}
