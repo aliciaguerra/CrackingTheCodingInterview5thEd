e798965c18d54cdcc5ae31a7591e77cbb563c9cc
Alicia Guerra <alicia.developer@aliciaguerra.com>
2016-04-03 21:35:43 -0700

    Update 1.5.java

--- 1.5.java
+++ 1.5.java
@@ -55,6 +55,29 @@ String compressBetter(String str) {
  }
  
  /*In lines 15-16 above, characters are inserted when the repeated character chanches. We need to update
- 
- */
+ the string at the end of the method as well, since the very last set of repeated characters wouldn't be
+ set in the compressed string of characters yet.*/
+ mystr.append(last);
+ mystr.append(count);
+ return mystr.toString();
+}
+
+int countCompression(String str) {
+ if(str==null || str.isEmpty()) return 0;
+ char last = str.charAt(0);
+ int size = 0;
+ int count =1;
+ for(int i=1; i<str.length(); i++) {
+ 	if(tr.charAt(i)==last){
+ 		count++;
+ 	} else {
+ 		last = str.charAt(i);
+ 		size += 1 +String.valueOf(count).length();
+ 		count = 1;
+ 	}
+ 	}
+ 	size += 1 + String.valueOf(count).length();
+ 	return size;
+ }
+}
  
