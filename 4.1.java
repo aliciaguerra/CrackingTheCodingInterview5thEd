72a0b13dfad1d18f3a4942f0bd868d905816427b
Alicia Guerra <alicia.developer@aliciaguerra.com>
2016-04-07 05:19:56 -0700

    Update 4.1.java

--- 4.1.java
+++ 4.1.java
@@ -19,3 +19,56 @@ public static boolean isBalanced(TreeNode root) {
   return isBalanced(root.left) && isBalanced(root.right);
    }
   }
+
+/*Although this works, it is not very efficient. On each node, we recurse through its entire subtree.  This means
+that getHeight is called repeatedly on the same nodes. The algorithm is O(NlogN) since each node is "touched" once
+per node above it. 
+
+We need to cut out some of the calls to getHeight.
+
+If we inspect this method, we may notice that getHeight could actually check if the tree is balanced as the same time 
+as its checking heights. What do we do when we discover that the subtree  isn't balanced? Just return -1.
+
+This improved algorithm works by checking each subtree as we recurse from down the root. On each node, we recursively
+get the heights of the left and right subtrees through the left and height methods. If the subtree is balanced, then
+checkHeight will return the actual height of the subtree. If the subtree is not balanced, then checkHeight will return -1.
+We will immediately break and return -1 from the current call.
+
+The code implements this algorithm. */
+
+public static int checkHeight(TreeNode root) {
+  if(root==null) {
+   return 0; //Height of 0
+  }
+
+/*Check if left is balanced.*/
+int leftHeight = checkHeight(root.left);
+if(leftHeight == -1) {
+ return -1; //Not balanced
+}
+
+/*Check if right is balanced.*/
+int rightHeight = checkHeight(root.right);
+if(rightHeight = checkHeight(root.right);
+return -1; //Not balanced
+}
+
+/*Check if the current node is balanced.*/
+int heightDiff = leftHeight - rightHeight;
+if(Math.abs(heightDiff)>1) {
+ return -1; //Not balanced
+} else {
+ /*Return height*/
+ return Math.max(leftHeight, rightHeight)+1;
+    }
+  }
+  
+ public static boolean isBalanced(TreeNode root) {
+  if(checkHeight(root)==-1) {
+   return flase;
+  }
+  else {
+   return true;
+  }
+ }
+}
